f55e64c8e321712bdb47b81818de86c16a2a6f96
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-14 15:17:49 +0200

    feat: bikeCatalog array of objects

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const bikeCatalog = [
+  {
+    model : `Btwin`,
+    weight : 15,
+  },
+  {
+    model : `Bianchi`,
+    weight : 12,
+  },
+  {
+    model : `Colnago`,
+    weight : 9,
+  },
+  {
+    model : `Legnano`,
+    weight : 14,
+  },
+  {
+    model : `Basso`,
+    weight : 16,
+  }
+]
