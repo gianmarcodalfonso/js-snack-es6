e26bcb574ea3e1006b080d097b6d67562c30243f
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-07-14 15:51:17 +0200

    feat: league array{team, points, sufferedFouls}

--- js/script.js
+++ js/script.js
@@ -21,6 +21,8 @@ const bikeCatalog = [
   }
 ]
 
+console.log(bikeCatalog)
+
 let lightest = bikeCatalog[0]
 
 for(let i = 1; i < bikeCatalog.length; i++){
@@ -31,3 +33,33 @@ for(let i = 1; i < bikeCatalog.length; i++){
 console.log(lightest)
 
 const rndmNum = (max) => Math.floor(Math.random() * max + 1 )
+
+const league = [
+  {
+    team : `Arsenal`,
+    points : 0,
+    sufferedFouls : 0,
+  },
+  {
+    team : `Juventus`,
+    points : 0,
+    sufferedFouls : 0,
+  },
+  {
+    team : `Real Mardid`,
+    points : 0,
+    sufferedFouls : 0,
+  },
+  {
+    team : `Bayern Munich`,
+    points : 0,
+    sufferedFouls : 0,
+  },
+  {
+    team : `Paris Saint-Germain`,
+    points : 0,
+    sufferedFouls : 0,
+  },
+]
+
+console.log(league)
